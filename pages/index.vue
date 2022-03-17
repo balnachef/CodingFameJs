46fbab8701a522906ee493a06dc52f6fdfd8fbff
balnachef <b.alnachef@smartinmedia.com>
2022-03-17 16:01:59 +0100

    added delete button for repo

--- pages/index.vue
+++ pages/index.vue
@@ -65,7 +65,7 @@
               </v-col>
               <v-col cols="3">
                 <v-btn
-                  v-if="index !== repos.length - 1"
+                  v-if="index !== repos.length - 1 || repos.length > 1"
                   class="mx-2"
                   fab
                   dark
